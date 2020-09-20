96c91e1bb1fec039091e16f599a97196cbbe1bd8
Igor APA <apa.igor@gmail.com>
2020-09-19 17:24:55 -0700

    Included more apps in cask list

--- install/brew-cask.sh
+++ install/brew-cask.sh
@@ -1,16 +1,20 @@
 apps=(
+    1password
     1password-cli
     appcleaner
     caffeine
     dbeaver-community
     docker
     dropbox
+    google-chrome
     insomnia
     skype
     slack
     spectacle
     spotify
+    telegram
     visual-studio-code
+    zoomus
 )
 
 brew cask install "${apps[@]}"
