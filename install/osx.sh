66bb2ebadca93686fe0882331653d9d6f05b5f22
Igor APA <apa.igor@gmail.com>
2020-09-19 17:56:29 -0700

    remove some brew commands

--- install/osx.sh
+++ install/osx.sh
@@ -1,9 +1,5 @@
 # Install Homebrew & brew-cask
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew tap caskroom/cask
-brew install brew-cask
-brew tap homebrew/versions
-brew tap caskroom/versions
 
 # Install brew & brew-cask packages
 . "$DOTFILES_DIR/install/brew.sh"
